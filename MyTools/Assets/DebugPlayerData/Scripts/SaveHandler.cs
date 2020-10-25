86c132ad92b28532fa8329ee8713397ae5630706
ironm102 <yunis.albreihi@gmail.com>
2020-10-25 11:22:32 +0100

    Rworked some of the debug data.

---
+++ MyTools/Assets/DebugPlayerData/Scripts/SaveHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveHandler : MonoBehaviour
+{
+    [SerializeField] private PlayerSave player;
+    
+    private PlayerData playerData = new PlayerData();
+    private IUnit unit;
+    private string jsonPath;
+    private string jsonFileName;
+    private string jsonFile;
+
+    private void Awake()
+    {
+        jsonPath = Application.dataPath + "/DebugStats/";
+        jsonFileName = player.name + "_" + DateTime.Now.Second + "_debugStats.json";
+        unit = player.GetComponent<IUnit>();
+        playerData.positions = new List<Vector3>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Save();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Load();
+        }
+    }
+
+    private void Save()
+    {
+        SaveJsonFile();
+        Debug.Log("Saved pos: " + playerData.positions[playerData.positions.Count - 1]);
+    }
+
+    private void Load()
+    {
+        LoadJsonFile();
+        StartCoroutine(MovePlayer());
+    }
+
+    IEnumerator MovePlayer() 
+    {
+        WaitForSeconds timeToWait = new WaitForSeconds(1.0f);
+        foreach (var pos in playerData.positions)
+        {
+            player.SetPosition(pos);
+            Debug.Log("Loaded pos: " + pos);
+            yield return timeToWait;
+        }
+    }
+
+    private void LoadJsonFile()
+    {
+        //Load
+        jsonFile = File.ReadAllText(jsonPath + jsonFileName);
+        playerData = JsonUtility.FromJson<PlayerData>(jsonFile);
+    }
+
+    private void SaveJsonFile() 
+    {
+        //Save
+        playerData.positions.Add(player.GetPosition());
+        jsonFile = JsonUtility.ToJson(playerData);
+        File.WriteAllText(jsonPath + jsonFileName, jsonFile);
+    }
+}
---
+++ MyTools/Assets/DebugPlayerData/Scripts/ShowPlayerDataTool.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class ShowPlayerDataTool : EditorWindow
+{
+    public PlayerSave player;
+
+    private Vector3 playerStartPosition;
+    private Vector3 playerPosition;
+
+    private PlayerData playerData;
+    private string jsonFile;
+    private string jsonPath;
+
+    [MenuItem("Tools/ShowPlayerData")]
+    static void Init() 
+    {
+        ShowPlayerDataTool window = (ShowPlayerDataTool)EditorWindow.GetWindow(typeof(ShowPlayerDataTool));
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+        jsonPath = Application.dataPath + "/debugStats.json";
+        player = FindObjectOfType<PlayerSave>();
+        InitiatePlayer();
+
+        SceneView.duringSceneGui += SceneGUI;
+    }
+
+    private void OnDisable()
+    {
+        SceneView.duringSceneGui -= SceneGUI;
+    }
+
+    private void OnGUI()
+    {
+        player = (PlayerSave)EditorGUILayout.ObjectField(player, typeof(PlayerSave),true);
+
+        if (GUI.changed == true)
+        {
+            InitiatePlayer();
+        }
+    }
+
+    private void Awake()
+    {
+
+    }
+
+    private void SceneGUI(SceneView sceneView) 
+    {
+        Handles.BeginGUI();
+
+        if (playerData != null)
+        {
+            for (int i = 0; i < playerData.positions.Count; i++)
+            {
+                if (i + 1 < playerData.positions.Count)
+                {
+                    Debug.DrawLine(playerData.positions[i], playerData.positions[i + 1]);
+                }
+            }
+        }
+
+        Handles.EndGUI();
+    }
+
+    private void LoadJsonFile() 
+    {
+        jsonFile = File.ReadAllText(jsonPath);
+        playerData = JsonUtility.FromJson<PlayerData>(jsonFile);
+    }
+
+    private void InitiatePlayer() 
+    {
+        LoadJsonFile();
+        playerStartPosition = player.transform.position;
+        playerPosition = playerStartPosition;
+    }
+}
